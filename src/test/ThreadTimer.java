28b8d64526a6a89266e858c164fcc2576f4dc80d
Matthieu Gaillard <matthieu.gaillard@neuf.fr>
2015-10-06 16:30:27 +0200

    GitHub c'est quand même vachement bien !

---
+++ src/test/ThreadTimer.java
@@ -0,0 +1,18 @@
+package test;
+
+public class ThreadTimer extends Thread {
+
+	public static int milli=0;
+	public void run(){
+		while(true){
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			milli++;
+		}
+	}
+
+}
---
+++ src/test/main.java
@@ -0,0 +1,15 @@
+package test;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ThreadTimer time = new ThreadTimer();
+		time.start();
+		Frame f = new Frame("bezier");
+
+	}
+
+}
