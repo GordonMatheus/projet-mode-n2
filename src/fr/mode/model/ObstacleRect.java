f3b36c086bb13e3afcec909125b7887f240b0989
Walenn <walen212@gmail.com>
2015-11-05 22:06:34 +0100

    Documentation

--- src/fr/mode/model/ObstacleRect.java
+++ src/fr/mode/model/ObstacleRect.java
@@ -4,6 +4,14 @@ import fr.mode.constantes.Constantes;
 
 public class ObstacleRect extends Obstacle {
 	
+	/** Constructeur de l'objet ObstacleRect, 
+	 * 	un Obstacle de forme rectangulaire.
+	 * 
+	 * @param double Coordonée sur l'axe x 	
+	 * @param double Coordonée sur l'axe y
+	 * @param int Largeur de l'obstacle (axe horizontal)
+	 * @param int Longueur de l'obstacle (axe vertical).
+	 */
 	public ObstacleRect(double pos0, double pos1, int height, int lenght) {
 		this.dimensions[0] = height ;
 		this.dimensions[1] = lenght ;
@@ -11,6 +19,13 @@ public class ObstacleRect extends Obstacle {
 		this.ObstaclePos[1]= pos1;
 	}
 
+	/** 
+	 * Calcule la distance entre les extrémités de l'obstacle rectangulaire 
+	 * et l'oiseau pour déterminer s'ils entrent en collision à partir de la 
+	 * distance entre les deux et le diamètre de l'oiseau et de l'obstacle.
+	 * 
+	 * @return boolean Vrai si la distance entre le bord de l'obstacle et l'oiseau est inférieure ou égale à 0, faux sinon.
+	 */
 	@Override
 	boolean collision() {
 		if(AngryBirdsModel.PlayerPos[0]<this.ObstaclePos[0] && AngryBirdsModel.PlayerPos[0]>(this.ObstaclePos[0]+dimensions[1]) )
